62c047b806bd64c86cfddee71b682fb93005b1fe
adjay27 <adityawhyjayaa@gmail.com>
2024-01-10 00:02:20 +0700

    fix search result

--- src/pages/TestSearch.jsx
+++ src/pages/TestSearch.jsx
@@ -1,27 +1,25 @@
 import { useParams } from "react-router-dom";
 import { Rating } from "@material-tailwind/react";
 import useGetData from "../useGetData";
+import { Link } from "react-router-dom";
 
-const ENDPOINT = 'https://bookapi.cm.hmw.lol/api/books';
+const ENDPOINT = "https://bookapi.cm.hmw.lol/api/books";
 
 export const TestSearch = () => {
-  const keyword = useParams().keyword
-  const {
-    data: books,
-    error,
-  } = useGetData(`${ENDPOINT}?search=${keyword}`);
+  const keyword = useParams().keyword;
+  const { data: books, error } = useGetData(`${ENDPOINT}?search=${keyword}`);
 
-  if (error) {
+  if (!books || !books.data) {
     return (
       <div className="w-full min-h-screen flex justify-center items-center">
-        <div className="text-3xl font-semibold">{error.message}</div>
+        <div className="text-3xl font-semibold">No search results found</div>
       </div>
     );
   }
 
-  const searchResult = books.data
+  const searchResult = books.data;
 
-  if (searchResult?.length < 1) {
+  if (searchResult.length < 1) {
     return (
       <div className="w-full min-h-[600px] flex justify-center items-center border border-accent rounded-lg">
         <div className="text-5xl text-accent font-semibold">
@@ -33,43 +31,43 @@ export const TestSearch = () => {
 
   return (
     <>
-      <div className="relative bg-[#f1f0fe] h-[147px] mx-9 my-4 rounded-[15px] grid content-center ">
-        <h1 className="text-4xl text-center font-bold">
+    <div className="relative bg-[#f1f0fe] h-[147px] mx-9 my-4 rounded-[15px] grid content-center">
+        <h1 className="text-4xl ml-[71px] font-bold">
           <strong className="text-bookoe">Search For</strong> {keyword}
         </h1>
       </div>
-      <div className="relative  mx-[32px] p-4 bg-[#f1f0fe] rounded-[15px] h-fit">
-        <div className="grid gap-y-[92px] gap-x-[114px] justify-center">
-          {searchResult?.map((book) => (
-              <div
-                className="book-wrap grid grid-cols-2 w-[564px] h-[346px]"
-                key={book.id}
-              >
-                <div className="img-container  h-full w-full grid justify-center">
-                  <img
-                    className="object-cover w-[277px] h-[346px] rounded-lg my-8"
-                    src={book.image_url}
-                    alt={book.title}
-                  />
-                </div>
+      <div className="backgroud-big relative w-max-fit h-max-fit top-[140px] mx-[32px] p-4 bg-[#f1f0fe] rounded-[15px] w-fit h-fit">
+        <div className="grid grid-cols-4 gap-x-[64px] justify-center ">
+          {searchResult.map((book, index) => (
+            <div
+              className="book-wrap h-[551px] w-[265px] grid grid-rows-2"
+              key={index}
+            >
+              <div className="img-container row-span-2 grid">
+                <img
+                  className="object-cover w-[260px] h-[340px] rounded-lg my-8"
+                  src={book.image_url}
+                  alt={book.title}
+                />
+              </div>
 
-                <div className="book-info py-8">
-                  <h1 className="font-bold text-xl truncate">{book.title}</h1>
-                  <p>by {book.author.name}</p>
-                  <div className="pt-4 flex justify-start">
-                    <div className="size-10 rounded-full bg-amber-400 p-2">
-                      {book.rating}
-                    </div>
-                    <Rating value={3} />
+              <div className="book-info grid w-[260px] ">
+                <h1 className="font-bold line-clamp-2 ">{book.title}</h1>
+                <p>by {book.author.name}</p>
+                <div className="flex justify-start ">
+                  <div className="size-10 rounded-full bg-amber-400 p-2">
+                    {book.rating}
                   </div>
-
-                  <p className="synopsis py-8">{book.synopsis}</p>
-                  <button className="block rounded-md w-[99px] py-2 border-solid border-2 border-[#8170F2] text-[#8170F2]">
+                  <Rating value={3} />
+                </div>
+                <Link to={`/books/${book.id}`}>
+                  <button className="block rounded-md py-2 w-full border-solid border-2 border-[#8170F2] text-[#8170F2]">
                     Read Book
                   </button>
-                </div>
+                </Link>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </>
